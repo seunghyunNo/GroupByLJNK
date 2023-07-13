66bb5e6702fa07b4175871341d6cb3748a8dbd79
seunghyunNo <qpqpqp9508@gmail.com>
2023-07-14 01:11:49 +0900

    boardRepository,boardService 내용 추가

--- src/main/java/com/project/controller/BoardController.java
+++ src/main/java/com/project/controller/BoardController.java
@@ -1,10 +1,34 @@
 package com.project.controller;
 
+import com.project.domain.Board;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
-    연습중
+//    @Autowired
+//    private Board board;
+//
+//
+//    @GetMapping("/detail/{id}")
+//    public String detail(){
+//        return "board/detail";
+//    }
+//    @GetMapping("/write")
+//    public void write(){}
+//
+//    @GetMapping("/update/{id}")
+//    public String update(){
+//        return "board/update";
+//    }
+//
+//    @PostMapping("/delete")
+//    public String deleteOk(){
+//
+//        return "board/deleteOk";
+//    }
 
 }
--- src/main/java/com/project/repository/boardRepository.java
+++ src/main/java/com/project/repository/boardRepository.java
@@ -1,5 +1,36 @@
 package com.project.repository;
 
+import com.project.domain.Board;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface boardRepository {
+
+    // 새글 작성
+    int write(Board board);
+
+    // 전체 글 목록
+    List<Board> list();
+
+    // id 글 내용 읽기
+    Board findById(Long id);
+
+    // 작성일
+    LocalDateTime regDate(Long id);
+
+    // 조회수
+    int viewCnt(Long id);
+
+    // 특정 id 글 수정
+    int update(Board board);
+
+    // 특정 id 글 삭제
+    int delete(Board board);
+
+
+    // 게시글 출력 갯수
+    List<Board> selectByPage(int from, int rows);
 
 }
