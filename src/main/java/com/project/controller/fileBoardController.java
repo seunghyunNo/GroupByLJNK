1f7b1c2caa348c9d0beba9a3bd2dae034c406660
hyunsoo922 <hskang9200@naver.com>
2023-07-14 21:48:59 +0900

    write 생성

--- src/main/java/com/project/controller/fileBoardController.java
+++ src/main/java/com/project/controller/fileBoardController.java
@@ -1,7 +1,19 @@
 package com.project.controller;
 
+import com.project.service.AttachmentService;
+import com.project.service.fileBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/fileboard")
 public class fileBoardController {
+
+    @Autowired
+    private fileBoardService fileBoardService;
+
+    @GetMapping("/write")
+    public void write(){}
 }
