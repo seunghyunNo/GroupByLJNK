88246751636f0edf47c1f35a3bf39a03a3acc7a8
june276 <jungjune276@gamil.com>
2023-07-15 23:56:08 +0900

    API 테스트

--- src/main/java/com/project/controller/ApiController.java
+++ src/main/java/com/project/controller/ApiController.java
@@ -1,8 +1,8 @@
 package com.project.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.service.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +13,9 @@ import java.net.URL;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+	@Autowired
+	private ApiService apiService;
+	
 	private final String API_KEY = "FB50E2191E8E06A7CA8BCC63648DEB93";
 	
 	@GetMapping("/gameList")
@@ -40,6 +43,30 @@ public class ApiController {
 		urlConnection.disconnect();
 		
 		return result.toString();
+	} // end callApi() gameList
+	
+	@PostMapping("/getAppData/{appId}")
+	public String getAppData(@PathVariable String appId) throws IOException {
+		// LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, String>>> temp
+		// 		= (LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, String>>>) apiService
+		// 		.getData("https://store.steampowered.com/api/appdetails?appids=" + appId + "&key=" + API_KEY)
+		// 		.getBody()
+		// 		;
+		// System.out.print("body : ");
+		// // System.out.println(temp.getClass());
+		// // System.out.println(temp);
+		// System.out.println("-".repeat(20));
+		// try{
+		// 	if(temp.get(appId).get("success").equals("true")){
+		// 		System.out.println(temp.get(appId).get("data"));
+		// 	}
+		// } catch (NullPointerException e){
+		// 	System.out.println("temp is null");
+		// } catch (Exception e){
+		// 	System.out.println("error");
+		// 	e.printStackTrace();
+		// }
 		
-	}
+		return "";
+	} // end getAppData
 }
---
+++ src/main/java/com/project/service/ApiService.java
@@ -0,0 +1,57 @@
+package com.project.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+
+@Service
+public class ApiService {
+	public ResponseEntity<Object> getData(String url){
+		//Spring restTemplate
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		ResponseEntity<Object> resultMap = new ResponseEntity<>(null,null,200);
+		
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			
+			HttpHeaders header = new HttpHeaders();
+			HttpEntity<?> entity = new HttpEntity<>(header);
+			
+			UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+			
+			resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Object.class);
+			
+			result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+			result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+			result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+			
+			//에러처리해야댐
+		} catch (HttpClientErrorException | HttpServerErrorException e) {
+			result.put("statusCode", e.getRawStatusCode());
+			result.put("body"  , e.getStatusText());
+			System.out.println("error");
+			System.out.println(e.toString());
+			
+			return resultMap;
+		}
+		catch (Exception e) {
+			result.put("statusCode", "999");
+			result.put("body"  , "excpetion오류");
+			System.out.println(e.toString());
+			
+			return resultMap;
+			
+		}
+		
+		return resultMap;
+	}
+}
