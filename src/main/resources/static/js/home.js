f7c8c43f3557b10b7073f8174b67ec2395075638
june276 <jungjune276@gamil.com>
2023-07-16 16:03:42 +0900

    클릭시 리뷰로 넘어가기

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -3,6 +3,7 @@ var api_key = "FB50E2191E8E06A7CA8BCC63648DEB93";
 $(function(){
 	$("#appList").children().html("<span>loading...</span>");
 	loadGameList();
+	addAppUrl();
 });
 
 function loadGameList(){
@@ -29,13 +30,15 @@ function parseJSON(data){
 		if(element.name.trim() != ''){
 			result.push(`
 			<div class="col-3 mb-3">
-				<img src="https://cdn.cloudflare.steamstatic.com/steam/apps/${appId}/hero_capsule.jpg" class="img-thumbnail">
-				<a th:href="@{/review/${appId}}">${element.name}</a>
+				<div class="container">
+				<img src="https://cdn.cloudflare.steamstatic.com/steam/apps/${appId}/hero_capsule.jpg"
+					class="img-thumbnail">
+				</div>
+				<a href="/review/${appId}">${element.name}</a>
 			</div>
 			`);
 		}
 	});
 	$appList.empty();
 	$appList.html(result.join('\n'));
-
 }
