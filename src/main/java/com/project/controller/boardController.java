de51acd36644cc836d8f4d16393e7f94b733ce6d
seunghyunNo <qpqpqp9508@gmail.com>
2023-07-18 19:25:14 +0900

    7/18 html write 작성중

--- src/main/java/com/project/controller/boardController.java
+++ src/main/java/com/project/controller/boardController.java
@@ -1,28 +1,65 @@
 package com.project.controller;
 
+import com.project.domain.Board;
+import com.project.service.BoardService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
-public class BoardController {
+public class boardController {
 
 
+    @Autowired
+    private BoardService boardService;
+    //
 //
-//
+    @GetMapping("/write")
+    public void write(){}
+
+    @PostMapping("/write")
+    public String writeOk(
+            @ModelAttribute("board")
+            @Valid
+            Board board,
+            BindingResult result,
+            Model model,
+            RedirectAttributes redirectAttributes
+    ) {
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("title", board.getTitle());
+            redirectAttributes.addFlashAttribute("content", board.getContent());
+
+            List<FieldError> errorList = result.getFieldErrors();
+            for(FieldError error : errorList){
+                redirectAttributes.addFlashAttribute("error_"+ error.getField(),error.getCode());
+            }
+        return "redirect:/board/write";
+        }
+        int write = boardService.write(board);
+        model.addAttribute("result",write);
+
+        return "board/writeOk";
+
+        }
+
+
+
+
+
     @GetMapping("/detail/{id}")
     public String detail(@PathVariable Long id,Model model){
 //        model.addAttribute()
         return "board/detail";
     }
-//    @GetMapping("/write")
-//    public void write(){}
-
-
 //
 //    @GetMapping("/update/{id}")
 //    public String update(){
--- src/main/java/com/project/service/AttachmentService.java
+++
@@ -1,4 +0,0 @@
-package com.project.service;
-
-public class AttachmentService {
-}
