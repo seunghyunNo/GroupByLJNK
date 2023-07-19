7f4de6ccee8ae83f06c0cfe175d05e0cc2e4e161
seunghyunNo <qpqpqp9508@gmail.com>
2023-07-19 19:20:54 +0900

    comment 작성중

--- src/main/java/com/project/controller/BoardCommentController.java
+++ src/main/java/com/project/controller/BoardCommentController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //data response 함
-@RequestMapping("/comment")
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController //data response 함
+@RequestMapping("/comment")
 public class BoardCommentController {
 
     @Autowired
--- src/main/java/com/project/domain/Comment.java
+++ src/main/java/com/project/domain/Comment.java
@@ -2,6 +2,7 @@ package com.project.domain;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
